98447ef291724681dbf656ca79e4f20c3e2da845
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-26 19:36:23 +0530

    Update start.py

--- RaiChu/Player/start.py
+++ RaiChu/Player/start.py
@@ -1,10 +1,7 @@
 import asyncio
 from time import time
 from datetime import datetime
-from Music import BOT_USERNAME
-from Music.config import UPDATES_CHANNEL, ZAID_SUPPORT
-from Music.MusicUtilities.helpers.filters import command
-from Music.MusicUtilities.helpers.command import commandpro
+from RaiChu.config import BOT_USERNAME, UPDATES_CHANNEL, ZAID_SUPPORT
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
