0ba74d31832c2c412dd2aa990cc2aecdc6b00209
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-01-28 14:39:20 +0530

    Update play.py

--- RaiChu/Player/play.py
+++ RaiChu/Player/play.py
@@ -24,7 +24,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from pytgcalls import StreamType
 from pytgcalls.types.input_stream import AudioPiped
 from youtubesearchpython import VideosSearch
-import youtube_dl
+import yt_dlp as youtube_dl
 
 FOREGROUND_IMG = [
     "Process/ImageFont/Red.png",
